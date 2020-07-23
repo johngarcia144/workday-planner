e6beb1fa54a8465334665966a3fc424f8966bac9
John Garcia <john.garcia144@gmail.com>
2020-07-23 14:31:09 -0600

    more html, with save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,30 @@
+
 $("#currentDay").append();
 
-  function currentDate() { 
-  $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
+function currentDate() { 
+$("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
   
-  } setInterval(currentDate, 1000);
- 
-
+} setInterval(currentDate, 1000);
   // Variables for save button, 
   //variable to current hour
   // variable current hourint
 
 
   // data attributes  for each hour so a color can be assigned
-  $("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+  // $("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
 // drilling into class, 
 // start jqueary
 
 // date and time in header
+ 
 
 // for loop, changing color to each row, adding in above attributes for each input line reflecting current hour
+  // if moment hour = #hour, then row = .present (orange)
+  // if moment hour = past, then row = .past (white)
+  // if moment hour = future, then row = .future (gren)
+
 
 // on click for save button that will save data
 
-// function to retrieve stored data (funciton (enter) for loop)
 
 
