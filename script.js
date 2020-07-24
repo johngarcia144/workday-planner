ddf66f87e234957949a1cb7d105286b0e5b758a3
John Garcia <john.garcia144@gmail.com>
2020-07-24 11:33:34 -0600

    data-id's added

--- script.js
+++ script.js
@@ -9,34 +9,34 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'));
   for(i = 9; i < 18; i++) {
     // variable for current hour
     var currentHour = parseInt(moment().format('HH')); 
-    // refers to 
+    // variable for teh row ID
     var rowHour = $("#hour-" + i);
-  
+    // if it's the currrent hour, the row will be red
     if (currentHour === i) {
       $("#hour-" + i).addClass("present");
     }
-
+    // if it's a past hour, the row will be grey
     if (currentHour > i) {
       $("#hour-" + i).addClass("past");
     }
-
+    //if the hour is in the future, the row will be green
     if (currentHour < i) {
       $("#hour-" + i).addClass("future");
     }
-
-    $("#hour-" + i).val(localStorage.getItem("hour-" + i))
+    //pulls data from local storage to view in textarea
+    $("#hour-" + i).val(localStorage.getItem("hour-" + i));
 
   }
 
-  // button to save data and retrieve it
+  // click function to save textarea data to local storage
 
-  // $(".saveBtn").on("click", function () {
-  //   ​
-  //     var id = $(this).attr("data-id")
+  $(".saveBtn").on("click", function () {
+    ​
+      var id = $(this).attr("data-id");
       
-  //     var value = $("#" + id).val()
-  //   ​
-  //     localStorage.setItem(id, value)
-  //   ​
-  //   });
+      var value = $("#" + id).val();
+    ​
+      localStorage.setItem(id, value);
+    ​
+    });
     
