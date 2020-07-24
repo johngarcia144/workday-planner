de472a992690af034fde7c7e2d473202106e8356
John Garcia <john.garcia144@gmail.com>
2020-07-24 11:15:05 -0600

    savebnt for loop breaking things

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // date and time in header
-moment(Date);
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+setInterval(() => {
+  moment(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm:ss a'));
+
+}, 1000)
 
 // for loop, changing color to each row
   for(i = 9; i < 18; i++) {
@@ -20,11 +23,20 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
     if (currentHour < i) {
       $("#hour-" + i).addClass("future");
     }
+
+    $("#hour-" + i).val(localStorage.getItem("hour-" + i))
+
   }
 
   // button to save data and retrieve it
 
-    $(".saveBtn").on("click", function() {
-      alert("Your event has been saved");
-    });
- 
+  // $(".saveBtn").on("click", function () {
+  //   ​
+  //     var id = $(this).attr("data-id")
+      
+  //     var value = $("#" + id).val()
+  //   ​
+  //     localStorage.setItem(id, value)
+  //   ​
+  //   });
+    
